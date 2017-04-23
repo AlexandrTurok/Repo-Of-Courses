406ae2399ecea06c0475dfa07ce4fba9082a378f
Алекс <aleks@iMac-Aleks.local>
2017-04-23 11:02:23 +0300

    Initial Commit

---
+++ firstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstApp
+//
+//  Created by Алекс  on 23.04.17.
+//  Copyright © 2017 Алекс . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
